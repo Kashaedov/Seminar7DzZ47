a8b5dc21aabb1047a5eaca0b8f72e1d81e254c95
Kashaedov Vitaliy <Vitalickashaedov@mail.ru>
2022-07-21 19:12:04 +0300

    Zadacha47

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿void Zadacha47()
+{
+    Random random = new Random();
+    int rows = random.Next(2,7);
+    int columns = random.Next(4,10);
+    double [,] array = new double [rows, columns];
+    FillArray(array);
+    PrintArray(array);
+
+    void FillArray(double[,] array)
+    {
+        Random random = new Random(); 
+        int rows = array.GetLength(0);
+        int columns = array.GetLength(1);
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+            array[i, j] = Math.Round(random.NextDouble() * 10 - 5, 1);
+            }
+        }
+    }
+
+    void PrintArray(double[,] array)
+    {
+        Console.WriteLine();
+        Console.WriteLine("Вывод массива");
+        int rows = array.GetLength(0);
+        int columns = array.GetLength(1);
+        for (int i = 0; i< rows; i++)
+        {
+            for (int j = 0; j < columns; j++ )
+                Console.Write(array [i, j] + "\t");
+                Console.WriteLine();
+        }
+    }
+}
+Zadacha47();
